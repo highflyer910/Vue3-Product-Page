34d6cdbab64740fae53cefa3c5d2c22a66190ea4
Thea <thea.mushambadze@gmail.com>
2020-08-16 01:10:02 +0400

    add components

--- components/productDisplay.js
+++ components/productDisplay.js
@@ -7,51 +7,49 @@ app.component('product-display', {
     },
     template:
     /*html*/
-        `<div class="product-display">
-      <div class="product-container">
-        <div class="product-image">
-          <img v-bind:src="image">
+    `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
         </div>
-        <div class="product-info">
-          <h1>{{ title }}</h1>
-  
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-  
-          <p>Shipping: {{ shipping }}</p>
-          <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-          </ul>
-  
-          <div 
-            v-for="(variant, index) in variants" 
-            :key="variant.id" 
-            @mouseover="updateVariant(index)" 
-            class="color-circle" 
-            :style="{ backgroundColor: variant.color }">
-          </div>
-          
-          <button 
-            class="button" 
-            :class="{ disabledButton: !inStock }" 
-            :disabled="!inStock" 
-            v-on:click="addToCart">
-            Add to Cart
-          </button>
-  
-          <!-- solution -->
-          <button 
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+        <button 
           class="button" 
           :class="{ disabledButton: !inStock }" 
           :disabled="!inStock" 
           @click="removeFromCart">
           Remove Item
         </button>
-        <!-- solution -->
-  
-        </div>
       </div>
-    </div>`,
+    </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
+  </div>`,
     data() {
         return {
             product: 'T-shirts',
@@ -63,21 +61,24 @@ app.component('product-display', {
                 { id: 2212, color: '#292D46', image: './assets/images/blue.jpg', quantity: 0 },
                 { id: 2213, color: '#2E2E2E', image: './assets/images/black.jpg', quantity: 5 },
                 { id: 2214, color: 'grey', image: './assets/images/grey.jpg', quantity: 22 },
-            ]
+            ],
+            reviews: []
+
         }
     },
     methods: {
         addToCart() {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
         },
-        // solution
         removeFromCart() {
             this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
         },
-        // solution
         updateVariant(index) {
             this.selectedVariant = index
-        }
+        },
+        addReview(review) {
+            this.reviews.push(review)
+          }
     },
     computed: {
         title() {
